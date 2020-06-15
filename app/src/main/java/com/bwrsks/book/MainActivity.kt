d788a9314987fbd4c266566290f80adbde58cc4d
Beta Wulandari <betawulandari21@gmail.com>
2020-06-15 23:57:57 +0700

    cabaca

---
+++ app/src/main/java/com/bwrsks/book/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.bwrsks.book
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.HttpURLConnection
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
---
+++ app/src/main/java/com/bwrsks/book/UpdateBook.kt
@@ -0,0 +1,4 @@
+package com.bwrsks.book
+
+class UpdateBook {
+}
